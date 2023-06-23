d9a00f8b0b66b59ca5c260aae9ca3659ed13d59e
John Burnette <john.burnette@revcontent.com>
2023-06-23 13:52:20 -0400

    Initial Commit

---
+++ SimpleWebViewExample/SimpleWebViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWebViewExampleApp.swift
+//  SimpleWebViewExample
+//
+//  Created by John Burnette on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWebViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
